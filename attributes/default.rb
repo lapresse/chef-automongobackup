a1db22893f3fd3662b748074a0551eb99de4709e
Jean-Francois Theroux <failshell@gmail.com>
2013-04-22 10:45:34 -0400

    initial commit?

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default[:automongobackup][:host] = 'localhost'
+default[:automongobackup][:backup_dir] = '/data/backups/mongodb'
+default[:automongobackup][:use_oplog] = 'no'
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'automongobackup'
+maintainer       'La Presse'
+maintainer_email 'jtheroux@lapresse.ca'
+license          'Apache 2.0'
+description      'Installs/Configures automongobackup'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
